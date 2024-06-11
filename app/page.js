53a53ec0e31e403f64bfe45f1d0b32a4fb9b065e
Zaid khan <zaidkhan.a888@gmail.com>
2024-06-11 15:17:19 +0530

    adding delete and add handler

--- app/page.js
+++ app/page.js
@@ -1,18 +1,66 @@
-import React from 'react';
 
+"use client"
+
+import React, { useState } from 'react';
+ 
 const page=()=>{
+  const [title,settitle]=useState("")
+  const [desc,setdesc]=useState("")
+  const [mainTask,setMainTask]=useState([])
+   const submitHandler=(e)=>{
+    e.preventDefault();
+setMainTask([...mainTask,{title,desc}])
+
+    // console.log(title);
+    settitle("");
+    setdesc("");
+   }
+   const deleteHandler=(i)=>{
+      let copytask=[...mainTask]
+      copytask.splice(i,1)
+      setMainTask(copytask)
+   }
+
+   let renderTask=<h2> No task Available</h2>
+    if(mainTask.length>0){
+    renderTask=mainTask.map((t,i)=>{
+      return (
+        <li key={i} className='flex items-center justify-between'>
+          <div className='flex items-center justify-between mb-5 w-2/3'>
+          <h5 className='text-2xl font-semibold'>{t.title}</h5>
+          <h6 className='text-xl font-semibold'>{t.desc}</h6>
+        </div>
+        <button
+        onClick={()=>{
+          deleteHandler(i)
+        }} 
+        className='bg-red-400 text-white px-4 py-2 rounded font-bold '>Delete</button>
+        </li>
+      )
+    })
+  }
+
   return(
     <>
       <h1 className='bg-black text-white p-5  text-5xl font-bold text-center '> Zaid Todo List</h1>
-      <form >
-       <input type='text' className='text-2xl border-zinc-800 border-2 m-8 px-4 py-2' placeholder='Enter task here'/>
+      <form onSubmit={submitHandler} >
+       <input type='text' className='text-2xl border-zinc-800 border-2 m-8 px-4 py-2' placeholder='Enter task here' value={title} onChange={(e)=>{
+        settitle(e.target.value)
+       }}/>
      
-       <input type='text' className='text-2xl border-zinc-800 border-2 m-8 px-4 py-2' placeholder='Enter description here here'/>
+       <input type='text' className='text-2xl border-zinc-800 border-2 m-8 px-4 py-2' placeholder='Enter description here here' value={desc} onChange={(e)=>{
+        setdesc(e.target.value)
+       }}/>
        <button className='bg-black text-white px-4 py-2 text-2xl font-bold m-5' > Add Task</button>
          
       </form>
     
-    
+  <hr/>
+  <div className='p-8 bg-slate-200'>
+    <ul>
+      {renderTask}
+    </ul>
+  </div>
     </>
   )
 } 
