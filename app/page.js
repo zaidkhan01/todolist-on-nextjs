2351f919ef4d1cd2e55711acfcc4361c82125aba
Zaid khan <zaidkhan.a888@gmail.com>
2024-06-11 15:25:21 +0530

    changing the name

--- app/page.js
+++ app/page.js
@@ -42,7 +42,7 @@ setMainTask([...mainTask,{title,desc}])
 
   return(
     <>
-      <h1 className='bg-black text-white p-5  text-5xl font-bold text-center '> Zaid Todo List</h1>
+      <h1 className='bg-black text-white p-5  text-5xl font-bold text-center '>Todo List</h1>
       <form onSubmit={submitHandler} >
        <input type='text' className='text-2xl border-zinc-800 border-2 m-8 px-4 py-2' placeholder='Enter task here' value={title} onChange={(e)=>{
         settitle(e.target.value)
